2dd4c47f780972386764fa973f41ed0af7251866
phetzzy99 <phetit_sru@hotmail.com>
2023-07-13 16:42:15 +0700

    4th Commit

---
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\DataTables\SubCategoryDataTable;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+use Str;
+
+class SubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(SubCategoryDataTable $dataTable)
+    {
+        return $dataTable->render('admin.sub-category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.sub-category.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category' => ['required'],
+            'name' => ['required', 'max:200', 'unique:sub_categories,name'],
+            'status' => ['required']
+        ]);
+
+        $subCategory = new SubCategory();
+
+        $subCategory->category_id = $request->category;
+        $subCategory->name = $request->name;
+        $subCategory->slug = Str::slug($request->name);
+        $subCategory->status = $request->status;
+        $subCategory->save();
+
+        toastr('Created Successfully!', 'success');
+
+        return redirect()->route('admin.sub-category.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
